50d74ca16d3941dae090d00aa22ff9d1e9dfef4c
Vali <vali.dvoyakin@gmail.com>
2026-03-29 01:05:11 +0300

    refactor: move database connection URL configuration from schema to prisma config file

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations"
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"]
   }
-  // datasource: {
-  //   url: process.env["DATABASE_URL"],
-  // },
 })
